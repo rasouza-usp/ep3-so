081b037475f6e75a52df7fc5b8cd2b9c8091c902
taispin <tais141@gmail.com>
2017-11-17 13:56:09 -0200

    incluindo estrutura de pagina

--- paging.py
+++ paging.py
@@ -1,5 +1,12 @@
 # algoritmos de substituicao de paginas
 
+class Page:
+    def __init__ (self, inicio, p):
+        self.tamanho = p                 #define o tamanho da pagina
+        self.inicio = int(inicio)        #onde a pagina comeca
+        self.fim = int(inicio) + p - 1   #onde a pagina termina
+        self.mapeada = 0                 #mapeada = 1 se a pagina esta mapeada na memoria fisica e 0 cc
+
 
 def optimal():
     print 'optimal'
